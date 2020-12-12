11693959ee2f1d65f54fcf2f0f29a0d46042c04d
Rafic El Kouche <raficelkouche@hotmail.com>
2020-12-11 21:03:27 -0500

    Added some methods

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) ? true : false
   }
 
   /** Stretch **/
@@ -36,9 +44,36 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    //Checks if "this" and the vampire are the same
+    if (this.name === vampire.name) {
+      return this;
+    }
+    //Checks if "this" or the vampire are a root node
+    if (!this.creator || !vampire.creator) {
+      return this.isMoreSeniorThan(vampire) ? this : vampire
+    }
+    //Checks if a direct ancestor is used
+    if (this.creator.name === vampire.name || vampire.creator.name === this.name) {
+      return this.isMoreSeniorThan(vampire) ? this : vampire
+    }
+    //For all other cases
+    let currentVampire = this;
+    const currentVampireParents = [];
 
+    while (currentVampire.creator) {                      //get all "this" vampires parents           
+      currentVampireParents.push(currentVampire.creator);
+      currentVampire = currentVampire.creator
+    };
+
+    while (vampire.creator) {                             //Check each vampire's parents against the other's parents
+      if (currentVampireParents.includes(vampire.creator)) {
+        return vampire.creator
+      }
+      vampire = vampire.creator;
+    }
+
+    return vampire;
   }
-}
+};
 
 module.exports = Vampire;
-
